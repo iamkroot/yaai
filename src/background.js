0c0b0b76d7b204859aedb9aa205c4d18358f8c74
iamkroot <kroot.patel@gmail.com>
2021-01-13 10:53:17 +0530

    Add dialog popup and finish Aria2 integration

--- src/background.js
+++ src/background.js
@@ -1,31 +1,94 @@
-import { copyObj, getFilename, getHeaderVal } from "./utils.js";
-import { Aria2 } from "aria2";
+import { getFilename, getHeaderVal } from "./utils.js";
+import { readAria2Options } from "./utils";
+const Aria2 = require("aria2");
 
-const aria = new Aria2();
 let requests = {};
 
 const REQD_HEADERS = ["Referer", "Cookie", "Cookie2", "Authorization"]
 
-const startDownload = async (respDetails, reqDetails) => {
-    let newReq = { ...reqDetails };
-
-    // get headers
-    if (reqDetails !== undefined) {
-        newReq.requestHeaders = copyObj(reqDetails.requestHeaders, REQD_HEADERS);
-    } else {
-        newReq.requestHeaders = "";
+const addToAria = async (params) => {
+    const url = params.url;
+    let args = {};
+    if (params.headers) {
+        // convert headers from array of {key: val} to array of "key: val"
+        args.header = params.headers.map(header => `${header.name}: ${header.value}`);
     }
-    newReq.fileName = getFilename(respDetails);
+    if (params.filename) {
+        args.out = params.filename;
+    }
+    if (params.dir && params.dir != "<default>") {
+        args.dir = params.dir;
+    }
+    let options = await readAria2Options();
+    const aria2 = new Aria2(options);
+    await aria2.call("addUri", [url], args);
+    return true;
 }
 
-const onSendHeaders = (details) => {
-    requests[details.requestId] = details;
+/**
+ * Get user's confirmation and start the download
+ * @param {any} respDetails Details of the headers response sent by server
+ * @param {any} reqDetails Details of request sent by browser to server
+ * @returns {boolean} true if browser download should be cancelled
+ */
+const startDownload = async (respDetails, reqDetails) => {
+    // get headers
+    let requestHeaders = "";
+    if (reqDetails !== undefined) {
+        requestHeaders = reqDetails.requestHeaders.filter(header => REQD_HEADERS.includes(header.name));
+    }
+
+    const filename = getFilename(respDetails);
+
+    // show the dialog to user
+    let cssUrl = browser.runtime.getURL("res/pure-min.css");
+    await browser.tabs.insertCSS(respDetails.tabId, {
+        file: cssUrl
+    });
+
+    let scriptUrl = browser.runtime.getURL("popup/index.js");
+    await browser.tabs.executeScript(respDetails.tabId, {
+        file: scriptUrl
+    });
+
+    const params = {
+        url: respDetails.url,
+        filename,
+        dir: "<default>",
+        headers: requestHeaders
+    };
+
+    const userChoice = await browser.tabs.sendMessage(respDetails.tabId, {
+        action: "confirmDownload",
+        params
+    });
+
+    await browser.tabs.removeCSS(respDetails.tabId, { file: cssUrl });
+
+    Object.assign(params, userChoice.params);
+    switch (userChoice.downloadMethod) {
+        case "aria2":
+            try {
+                return await addToAria(params);
+            } catch (error) {
+                console.error("error from aria", error);
+                return false;
+            }
+        case "firefox":
+            return false;
+        case "halt":
+            return true;
+        default:
+            console.warn("Unknown method", userChoice);
+            break;
+    }
+    return false;
 }
 
 const COMMON_TYPES = ["pdf", "xhtml", "x-xpinstall", "x-shockwave-flash", "rss", "json"];
 
 
-const shouldHijackDownload = (details) => {
+const shouldInterceptDownload = (details) => {
     if (details.statusCode != 200) {
         return false;
     }
@@ -42,12 +105,16 @@ const shouldHijackDownload = (details) => {
 }
 
 const onHeadersReceived = (details) => {
-    try {
-        if (shouldHijackDownload(details)) {
-            startDownload(details, requests[details.requestId]);
-            return { cancel: true };
-        }
-    } finally {
+    if (shouldInterceptDownload(details)) {
+        return new Promise(resolve => {
+            startDownload(details, requests[details.requestId]).then(shouldCancel => {
+                if (shouldCancel)
+                    resolve({ cancel: true });
+                else
+                    resolve();  // no change
+            }).finally(() => delete requests[details.requestId]);
+        });
+    } else {
         delete requests[details.requestId];
     }
     return;  // no change
@@ -56,7 +123,9 @@ const onHeadersReceived = (details) => {
 const RES_TYPES = ["main_frame", "sub_frame"];
 
 browser.webRequest.onSendHeaders.addListener(
-    onSendHeaders,
+    (details) => {
+        requests[details.requestId] = details;
+    },
     {
         urls: ["<all_urls>"],
         types: RES_TYPES
--- src/popup.js
+++ src/popup.js
@@ -10,8 +10,54 @@ const insertDialog = async () => {
     const url = browser.runtime.getURL("popup/dialog.html");
     const html = await fetch(url);
     const text = await html.text();
-    let el = document.createElement("slot");
-    el.innerHTML = text;
-    document.documentElement.appendChild(el);
-    return el.firstChild;
+    document.body.insertAdjacentHTML("beforeend", text);
+    return document.getElementById("yaai-dialog");
 }
+
+/**
+ * Add values to the dialog fields
+ * @param {any} params 
+ * @param {HTMLDialogElement} dialog 
+ */
+const populateDialog = (params, dialog) => {
+    for (const param of ["url", "filename", "dir"]) {
+        document.getElementById("yaai-" + param).value = params[param] || "";
+    }
+    dialog.returnValue = "";
+    dialog.removeAttribute("open");
+}
+
+const DEFAULT_METHOD = "firefox";
+
+/**
+ * Retrieve the form values and user selection from dialog
+ * @param {HTMLDialogElement} dialog 
+ */
+const getUserInput = (dialog) => new Promise((resolve) => {
+    dialog.addEventListener("close", () => {
+        let params = {};
+        for (const param of ["url", "filename", "dir"]) {
+            params[param] = document.getElementById("yaai-" + param).value || "";
+        }
+        const val = dialog.returnValue || DEFAULT_METHOD;
+        populateDialog({}, dialog); // clear the dialog for future use.
+        resolve({ params, downloadMethod: val });
+    }, { once: true });
+})
+
+/**
+ * Show a popup and get user's confirmation
+ * @param {any} params The details of the intercepted download
+ */
+const confirmDownload = async (params) => {
+    const dialog = await insertDialog();
+    populateDialog(params, dialog);
+    dialog.showModal();
+    return await getUserInput(dialog);
+}
+
+browser.runtime.onMessage.addListener((data) => {
+    if (data.action === "confirmDownload") {
+        return confirmDownload(data.params);
+    }
+})
